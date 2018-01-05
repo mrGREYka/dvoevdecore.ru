271b52b85744e5e6c59b42ee11e4e7690cde6948
Your Name <you@example.com>
2018-01-06 02:23:19 +0300

    menu

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -89,7 +89,7 @@ AppAsset::register($this);
                 <div class="row">
                     <div class="col-md-2 col-sm-2 col-xs-4">
                         <div class="logo">
-                            <a href="index.html">
+                            <a href="/">
                                 <img src="/images/logo/logo.png" alt="">
                             </a>
                         </div>
@@ -103,117 +103,14 @@ AppAsset::register($this);
                         </div>
                         <div class="main-menu">
                             <ul>
-                                <li class="active has-child"><a href="#">HOME</a>
-                                    <ul class="sub-menu">
-                                        <li><a href="index.html">Home One</a></li>
-                                        <li><a href="index-home2.html">Home Two</a></li>
-                                        <li><a href="index-home-3.html">Home Three</a></li>
-                                    </ul>
-                                </li>
-                                <li class="has-child"><a href="#">ABOUT US </a>
-                                    <ul class="sub-menu">
-                                        <li><a href="about-one.html">About One</a></li>
-                                        <li><a href="about-two.html">About Two</a></li>
-                                    </ul>
-                                </li>
-                                <li class="has-child"><a href="#">PORFOLIO </a>
-                                    <ul class="sub-menu">
-                                        <li class="has-child"><a href="#!">masonry</a>
-                                            <ul class="menu-sub">
-                                                <li><a href="portfolio_1.html">Portfolio 1</a></li>
-                                                <li><a href="portfolio_2.html">Portfolio 2</a></li>
-                                                <li><a href="portfolio_4.html">Portfolio 3</a></li>
-                                                <li><a href="portfolio_05.html">Portfolio 4</a></li>
-                                            </ul>
-                                        </li>
-                                        <li class="has-child"><a href="#!">Normal</a>
-                                            <ul class="menu-sub">
-                                                <li><a href="portfolio_3.html">portfolio 1</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="has-child"><a href="#">SERVICE </a>
-                                    <ul class="sub-menu">
-                                        <li><a href="services-one.html">service One</a></li>
-                                        <li><a href="services-two.html">service Two</a></li>
-                                        <li><a href="services-elements.html">Service Elements</a></li>
-                                    </ul>
-                                </li>
-                                <li class="has-child"><a href="#">BLOG </a>
-                                    <ul class="sub-menu">
-                                        <li><a href="blog-grid.html">Blog Grid</a></li>
-                                        <li><a href="blog-leftsidebar.html">Blog left Sidebar</a></li>
-                                        <li><a href="blog-right-sidebar.html">Blog right Sidebar</a></li>
-                                        <li><a href="single-blog-full-width.html">single full width</a></li>
-                                        <li><a href="single-blog-left-sidebar.html">single left sidebar</a></li>
-                                        <li><a href="single-blog.html">single Right sidebar</a></li>
-                                    </ul>
-                                </li>
-                                <li class="has-child"><a href="#">PAGES </a>
-                                    <ul class="mega-menu">
-                                        <li>
-                                            <h3>Elements <span class="clr-span">One</span></h3>
-                                            <a href="accrodian.html">Accordion/Toggle</a>
-                                            <a href="alerts.html">Alerts</a>
-                                            <a href="button.html">Button</a>
-                                            <a href="tab.html">tab</a>
-                                            <a href="social-icon.html">Social Icon</a>
-                                            <a href="typography.html">typography</a>
-                                            <a href="form.html">Form</a>
-
-                                        </li>
-                                        <li>
-                                            <h3>Elements <span class="clr-span">Two</span></h3>
-                                            <a href="services-elements.html">Services</a>
-                                            <a href="all-tem.html">tem</a>
-                                            <a href="carosel.html">Carosel</a>
-                                            <a href="call-to-action.html">call to action</a>
-                                            <a href="subscribe.html">Subscribe</a>
-                                            <a href="funfact.html">Fun Fact</a>
-                                            <a href="gallery.html">gallery</a>
-                                        </li>
-                                        <li>
-                                            <h3>Elements <span class="clr-span">Three</span></h3>
-                                            <a href="main-shop.html">Shop page <span class="label label-primary">new</span></a>
-                                            <a href="shop-list.html">shop list view <span class="label label-primary">new</span></a>
-                                            <a href="shop-grid.html">shop grid view <span class="label label-primary">new</span></a>
-                                            <a href="shop-details.html">shop details <span class="label label-primary">new</span></a>
-                                            <a href="shopping-cart.html">cart <span class="label label-primary">new</span></a>
-                                            <a href="checkout.html">checkout <span class="label label-primary">new</span></a>
-                                            <a href="my-account.html">account <span class="label label-primary">new</span></a>
-
-                                        </li>
-                                        <li>
-                                            <h3>Elements <span class="clr-span">Four</span></h3>
-                                            <a href="comming-soon.html">Comming Soon <span class="label label-primary">EX</span></a>
-                                            <a href="events.html">Event <span class="label label-primary">EX</span></a>
-                                            <a href="single-events.html">Event details <span class="label label-primary">EX</span></a>
-                                            <a href="faq_left-seidebar.html">Faq left sidebar</a>
-                                            <a href="faq_right_sidebar.html">Faq Right sidebar</a>
-                                            <a href="progressbar.html">progressbar</a>
-                                            <a href="price-table.html">price table</a>
-                                        </li>
-                                        <li>
-                                            <h3>Elements <span class="clr-span">five</span></h3>
-                                            <a href="project_detials.html">project details</a>
-                                            <a href="404.html">404</a>
-                                            <a href="team-one.html">team one</a>
-                                            <a href="team-two.html">team two</a>
-                                            <a href="single-team.html">single team</a>
-                                            <a href="services-one.html">Service one</a>
-                                            <a href="services-two.html">Service two</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="has-child"><a href="#">CONTACT </a>
-                                    <ul class="sub-menu">
-                                        <li><a href="contact.html">contact us 1</a></li>
-                                        <li><a href="contactus_2.html">contact us 2</a></li>
-                                    </ul>
-                                </li>
+                                <li class="active"><a href="#">О нас</a></li>
+                                <li><a href="#">Наши услуги</a></li>
+                                <li><a href="#">Портфолио</a></li>
+                                <li><a href="#">Отзывы</a></li>
+                                <li><a href="#">Контакты</a></li>
                             </ul>
                         </div>
+
                     </div>
                     <!-- end main menu -->
                     <div class="col-md-1 col-sm-1 col-xs-4">

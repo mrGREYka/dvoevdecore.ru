85217d148957e8c8abbd8d4b3c90763d0f09b872
Your Name <you@example.com>
2018-01-06 02:13:49 +0300

    mainPage

--- views/site/index.php
+++ views/site/index.php
@@ -6,4 +6,342 @@ $this->title = 'Двое в декоре';
 ?>
 <!--content-->
 
+ <!-- 
+========================================
+STEP PORTFOLIO START  FORM HERE
+========================================
+-->
+    <div class="step-portfolio-area section-pading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="main-section-heading">Наши проекты</h2>
+                    </div>
+                </div>
+                <!-- end section title -->
+            </div>
+            <!-- end row -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="filter-menu">
+                        <ul class="filter-list text-center">
+                            <li data-filter="*" class="active btn-mr waves-effect waves-teal  white">All</li>
+                            <li data-filter=".cat-1" class="btn-mr waves-effect waves-teal  white">Design</li>
+                            <li data-filter=".cat-2" class="btn-mr waves-effect waves-teal  white">Web</li>
+                            <li data-filter=".cat-3" class="btn-mr waves-effect waves-teal  white">Indentity</li>
+                            <li data-filter=".cat-4" class="btn-mr waves-effect waves-teal  white">Interface</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <!-- end row -->
+            <div class="big-spacer2"></div>
+            <!-- end bigspacer -->
+            <div class="row">
+                <div class="grid">
+                    <div class="col-sm-4 col-xs-12 grid-item cat-1 cat-4">
+                        <div class="single-port">
+                            <img src="/images/portfolio/port-04/pp1.jpg" alt="">
+                            <div class="port-hover waves-effect waves-light-1">
+                                <a href="project_detials.html"><i class="fa fa-search"></i></a>
+                                <h5>Branding Project</h5>
+                            </div>
+                            <!-- end hover project -->
+                        </div>
+                    </div>
+                    <!-- end single port -->
+                    <div class="col-sm-4 col-xs-12 grid-item cat-2 cat-3 cat-4 cat-1">
+                        <div class="single-port">
+                            <img src="/images/portfolio/port-04/pp2.jpg" alt="">
+                            <div class="port-hover waves-effect waves-light-1">
+                                <a href="project_detials.html"><i class="fa fa-search"></i></a>
+                                <h5>Branding Project</h5>
+                            </div>
+                            <!-- end hover project -->
+                        </div>
+                    </div>
+                    <!-- end single port -->
+                    <div class="col-sm-4 col-xs-12 grid-item cat-4 cat-3">
+                        <div class="single-port">
+                            <img src="/images/portfolio/port-04/pp3.jpg" alt="">
+                            <div class="port-hover waves-effect waves-light-1">
+                                <a href="project_detials.html"><i class="fa fa-search"></i></a>
+                                <h5>Branding Project</h5>
+                            </div>
+                            <!-- end hover project -->
+                        </div>
+                    </div>
+                    <!-- end single port -->
+                    <div class="col-sm-4 col-xs-12 grid-item cat-4 cat-2">
+                        <div class="single-port">
+                            <img src="/images/portfolio/port-04/pp4.jpg" alt="">
+                            <div class="port-hover waves-effect waves-light-1">
+                                <a href="project_detials.html"><i class="fa fa-search"></i></a>
+                                <h5>Branding Project</h5>
+                            </div>
+                            <!-- end hover project -->
+                        </div>
+                    </div>
+                    <!-- end single port -->
+                    <div class="col-sm-4 col-xs-12 grid-item cat-2 cat-3">
+                        <div class="single-port">
+                            <img src="/images/portfolio/port-04/pp5.jpg" alt="">
+                            <div class="port-hover waves-effect waves-light-1">
+                                <a href="project_detials.html"><i class="fa fa-search"></i></a>
+                                <h5>Branding Project</h5>
+                            </div>
+                            <!-- end hover project -->
+                        </div>
+                    </div>
+                    <!-- end single port -->
+                    <div class="col-sm-4 col-xs-12 grid-item cat-1 cat-2 cat-3">
+                        <div class="single-port">
+                            <img src="/images/portfolio/port-04/pp6.jpg" alt="">
+                            <div class="port-hover waves-effect waves-light-1">
+                                <a href="project_detials.html"><i class="fa fa-search"></i></a>
+                                <h5>Branding Project</h5>
+                            </div>
+                            <!-- end hover project -->
+                        </div>
+                    </div>
+                    <!-- end single port -->
+                    <div class="col-sm-4 col-xs-12 grid-item cat-3 cat-4 cat-2">
+                        <div class="single-port">
+                            <img src="/images/portfolio/port-04/pp7.jpg" alt="">
+                            <div class="port-hover waves-effect waves-light-1">
+                                <a href="project_detials.html"><i class="fa fa-search"></i></a>
+                                <h5>Branding Project</h5>
+                            </div>
+                            <!-- end hover project -->
+                        </div>
+                    </div>
+                    <!-- end single port -->
+                </div>
+            </div>
+            <!-- end row -->
+        </div>
+    </div>
+    <!-- 
+========================================
+STEP PORTFOLIO AREA END FORM HERE
+========================================
+-->
+
+<!-- 
+========================================
+SERVICES
+========================================
+  -->
+<div class="elemnts-services section-pading2">
+    <div class="container">
+        <div class="row">
+            <h2 class="elemnt-heading mtb-30 text-center">Наши услуги</span>
+            </h2>
+            <div class="col-md-3 col-sm-6 col-xs-12 stb-30 mbt-30">
+                <div class="home-3-single-services">
+                    <i class="fa fa-cogs"></i>
+                    <h5><span>EASY CUSTOMIZE</span></h5>
+                </div>
+            </div>
+            <!-- end single services -->
+            <div class="col-md-3 col-sm-6 col-xs-12 stb-30 mbt-30">
+                <div class="home-3-single-services">
+                    <i class="fa fa-comments"></i>
+                    <h5><span>BUSINESS CONSULTING</span></h5>
+                </div>
+            </div>
+            <!-- end single services -->
+            <div class="col-md-3 col-sm-6 col-xs-12 mbt-30">
+                <div class="home-3-single-services">
+                    <i class="fa fa-users"></i>
+                    <h5><span>Degital Strategy</span></h5>
+                </div>
+            </div>
+            <!-- end single services -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+                <div class="home-3-single-services">
+                    <i class="fa fa-pencil"></i>
+                    <h5><span>Degital Strategy</span></h5>
+                </div>
+            </div>
+            <!-- end single services -->
+        </div>
+        <div class="big-spacer"></div>
+    </div>
+</div>
+<!-- 
+========================================
+SERVICES
+========================================
+  -->        
+
+ <!-- 
+========================================
+STEP SERVICES AREA START FORM HERE
+========================================
+-->
+
+    <div class="step-services section-pading home-two about-page">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="main-section-heading">Наши преимущества</h2>
+                    </div>
+                </div>
+                <!-- end section title -->
+            </div>
+            <div class="row">
+                <div class="col-md-4 mp-0 col-sm-6 col-xs-12  mbt-30">
+                    <div class="step-single-services waves-effect waves-light-1">
+                        <i class="fa fa-briefcase"></i>
+                        <h2 class="services-title2">Professional Team</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor labore dolore magna aliqua. </p>
+                    </div>
+                </div>
+                <!-- end single services -->
+                <div class="col-md-4 mp-0 col-sm-6 col-xs-12  mbt-30">
+                    <div class="step-single-services waves-effect waves-light-1">
+                        <i class="fa fa-pencil"></i>
+                        <h2 class="services-title2">Refund Policy</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor labore dolore magna aliqua. .</p>
+                    </div>
+                </div>
+                <!-- end single services -->
+                <div class="col-md-4 mp-0 col-sm-6 col-xs-12  mbt-30">
+                    <div class="step-single-services waves-effect waves-light-1">
+                        <i class="fa fa-map-marker"></i>
+                        <h2 class="services-title2">Remote Support</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor labore dolore magna aliqua. .</p>
+                    </div>
+                </div>
+                <!-- end single services -->
+                <div class="col-md-4 mp-0 col-sm-6 col-xs-12  mbt-30">
+                    <div class="step-single-services waves-effect waves-light-1">
+                        <i class="fa fa-share-alt"></i>
+                        <h2 class="services-title2">Social Media Support</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor labore dolore magna aliqua. .</p>
+                    </div>
+                </div>
+                <!-- end single services -->
+                <div class="col-md-4 mp-0 col-sm-6 col-xs-12 mbt-30">
+                    <div class="step-single-services waves-effect waves-light-1">
+                        <i class="fa fa-envelope"></i>
+                        <h2 class="services-title2">Email Support</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor labore dolore magna aliqua. .</p>
+                    </div>
+                </div>
+                <!-- end single services -->
+                <div class="col-md-4 mp-0 col-sm-6 col-xs-12">
+                    <div class="step-single-services waves-effect waves-light-1">
+                        <i class="fa fa-clock-o"></i>
+                        <h2 class="services-title2">24/7 Support</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor labore dolore magna aliqua. .</p>
+                    </div>
+                </div>
+                <!-- end single services -->
+            </div>
+        </div>
+    </div>
+    <!-- 
+========================================
+STEP SERVICES AREA END FORM HERE
+========================================
+--> 
+
+<!-- 
+========================================
+GET IN TOUCH TWO AREA START FORM HERE
+========================================
+-->
+    <div class="step-get-in-touch section-pading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="section-heading2">Получить консультацию</h2>
+                    </div>
+                </div>
+                <!-- end section title -->
+            </div>
+            <!-- end row -->
+            <div class="row">
+                    <div class="contact-address">
+                            <div class="col-md-3 col-md-offset-2 col-sm-4 col-xs-12 mbt-30">
+                                <div class="single-contact-address">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <div class="con-i">
+                                            <i class="fa fa-phone"></i>
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <h5>+088-555-222-44</h5>
+                                        <h5>+099-777-333-55</h5>
+                                    </div>
+
+                                </div>
+                            </div>
+                            </div>
+                            <!-- end single contact address -->
+                            <div class="col-md-3 col-sm-4 col-xs-12 mbt-30">
+                                <div class="single-contact-address">
+                                <div class="media">
+                                    <div class="pull-left">
+                                       <div class="con-i">
+                                            <i class="fa fa-paper-plane"></i>
+                                       </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <h5><a href="mailto:iinfo@gmail.com">info@gmail.com</a></h5>
+                                        <h5><a href="mailto:www.yourweb.com">www.yourweb.com</a></h5>
+                                    </div>
+
+                                </div>
+                            </div>
+                            </div>
+                            <!-- end single contact address -->
+                            <div class="col-md-3 col-sm-4 col-xs-12">
+                               <div class="single-contact-address">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <div class="con-i">
+                                            <i class="fa fa-home"></i>
+                                        </div>
+                                    </div>
+                                    <div class="media-body">
+                                        <h5>212 , Inner circular road,</h5>
+                                        <h5>New York City, U.S.A.</h5>
+                                    </div>
+
+                                </div>
+                            </div> 
+                            </div>
+                            <!-- end single contact address -->
+                    </div>
+            </div> <!-- end row -->
+            <div class="big-spacer2"></div> <!-- end big spacer -->
+            <div class="row">
+                <form action="#" class="main-contact-form-contact">
+                    <div class="col-md-12">
+                        <input type="text" placeholder="Ваше имя">
+                        <input type="email" placeholder="Email">
+                        <textarea placeholder="Ваш вопрос"></textarea>
+                        <div class="text-right">
+                            <a href="#!" class="btn-mr waves-effect waves-light">Отправить</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- 
+========================================
+GET IN TOUCH TWO AREA START FORM HERE
+========================================
+--> 
+
 

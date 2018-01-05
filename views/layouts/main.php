10ab5ae70de2469ecbc66201c2405030fa1b1d14
Your Name <you@example.com>
2018-01-06 01:28:39 +0300

    favicon

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -22,6 +22,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>

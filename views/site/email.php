4d1f37aef6f0e6ce471b9340da4aa0937f902c9b
Your Name <you@example.com>
2018-01-08 16:43:36 +0300

    message

--- views/site/email.php
+++ views/site/email.php
@@ -6,7 +6,7 @@ use yii\helpers\Url;
     <h1 style="color: #f2b702;text-transform:uppercase;font-size:20px;text-align: center;">Сообщение с формы "Получить консультацию"</h1>
     <p style="color: black;text-align: center;"><span style="font-weight:bold">От: </span><?= Html::encode($model->name) ?></p>
     <p style="color: black;text-align: center;"><span style="font-weight:bold">Почта: </span><?= Html::encode($model->email) ?></p>
-    <p style="color: black;text-align: center;"><span style="font-weight:bold">Комментарий: </span><?= Html::encode($model->body) ?></p>
+    <p style="color: black;text-align: center;"><span style="font-weight:bold">Сообщение: </span><?= Html::encode($model->body) ?></p>
     <p style="color: black; text-align: center;">-----------------------</p>
     <p style="color: #f2b702;font-weight:bold;text-align:center;">это сообщение отправлено с сайта <?= Html::a('dvoevdecore.ru', Url::home('http')) ?></p>
 </div>

209db662506ebad01b66894374660069ecdfef1e
Your Name <you@example.com>
2018-01-18 01:05:18 +0300

    singleproject

--- views/site/project-single.php
+++ views/site/project-single.php
@@ -14,7 +14,6 @@ PROJECTS DETIALS AREA START FORM HERE
 ========================================
 -->
 
-
     <div class="stepproject-area section-pading2">
         <div class="container">
             <div class="row">
@@ -53,23 +52,192 @@ PROJECTS DETIALS AREA START FORM HERE
                     <a href="#!" class="btn-mr waves-effect waves-light">VIEW LIVE</a>
                 </div>
             </div>
-            <div class="big-spacer"></div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="text-center">
-                            <iframe width="854" height="480" src=<?= Html::encode("{$model->iframe}") ?> frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-                   
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
-
-
-
-
     <!-- 
 ========================================
 PROJECTS DETIALS AREA END FORM HERE
 ========================================
 -->
+
+<div class="step-amazing-gallery section-pading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="main-section-heading">Фотогаллерея</h2>
+                    </div>
+                </div>
+                <!-- end section title -->
+            </div>
+        </div>
+        <!-- end container -->
+        <div class="container">
+            <div class="step-gallery">
+                <ul>
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                    <li class="single-gallery-step">
+                        <img src="/images/gallery/gl1.jpg"" alt="">
+                        <div class="gallery-view">
+                            <a class="img-poppu" href="/images/gallery/gl1.jpg"">
+                                <i class="fa fa-search"></i>
+                            </a>
+                        </div>
+                    </li>
+                    <!-- end single gallery -->
+                </ul>
+            </div>
+        </div>    
+        <!-- end gallery  -->
+    </div>
+
+    <!-- 
+========================================
+STEP TEAMONE AREA END FORM HERE
+========================================
+-->
+
+<?php if ( trim( $model->iframe ) != '' ): ?>
+    <div class="step-video section-pading">  
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="main-section-heading">Видео со свадьбы</h2>
+                    </div>
+                </div>
+                <!-- end section title -->
+            </div>
+        </div>  
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="text-center">
+                        <iframe width="100%" height="600" src=<?= Html::encode("{$model->iframe}") ?> frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>    
+<?php endif; ?> 
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="text-center">
+                <div class="load-more text-center">
+                    <a href="/portfolio" class="btn-mr waves-effect waves-light">Все проекты</a>
+                </div>
+                <div class="big-spacer"></div>
+            </div>
+        </div>
+    </div>    
+</div>        
+    

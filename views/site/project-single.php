3cd9710b52dc419c050f7f19d7d36a9a7478fad3
Your Name <you@example.com>
2018-01-23 23:08:08 +0300

    singleproject

--- views/site/project-single.php
+++ views/site/project-single.php
@@ -14,42 +14,44 @@ PROJECTS DETIALS AREA START FORM HERE
 ========================================
 -->
 
+    <div class="row">
+        <div class="col-md-12 stb-30 mbt-30">
+            <div class="projects-detials-active owl-carousel">
+                <div class="single-project-detilas">
+                    <img src="/images/portfolio/project-detils/1.jpg" alt="">
+                </div>
+                <!-- end single projects -->
+                <div class="single-project-detilas">
+                    <img src="/images/portfolio/project-detils/2.jpg" alt="">
+                </div>
+                <!-- end single projects -->
+                <div class="single-project-detilas">
+                    <img src="/images/portfolio/project-detils/1.jpg" alt="">
+                </div>
+                <!-- end single projects -->
+                <div class="single-project-detilas">
+                    <img src="/images/portfolio/project-detils/2.jpg" alt="">
+                </div>
+                <!-- end single projects -->
+            </div>
+            <!-- end projects details active -->
+        </div>
+        <div class="col-md-12">
+            <div class="project-details-text">
+                <h3><?= Html::encode("{$model->title}") ?></h3>
+                <p><?= Html::encode("{$model->body}") ?></p>
+            </div>
+        </div>
+    </div>
     <div class="stepproject-area section-pading2">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 stb-30 mbt-30">
-                    <div class="projects-detials-active owl-carousel">
-                        <div class="single-project-detilas">
-                            <img src="/images/portfolio/project-detils/1.jpg" alt="">
-                        </div>
-                        <!-- end single projects -->
-                        <div class="single-project-detilas">
-                            <img src="/images/portfolio/project-detils/2.jpg" alt="">
-                        </div>
-                        <!-- end single projects -->
-                        <div class="single-project-detilas">
-                            <img src="/images/portfolio/project-detils/1.jpg" alt="">
-                        </div>
-                        <!-- end single projects -->
-                        <div class="single-project-detilas">
-                            <img src="/images/portfolio/project-detils/2.jpg" alt="">
-                        </div>
-                        <!-- end single projects -->
-                    </div>
-                    <!-- end projects details active -->
-                </div>
-                <div class="col-md-6">
-                    <div class="project-details-text">
-                        <h3><?= Html::encode("{$model->title}") ?></h3>
-                        <p><?= Html::encode("{$model->body}") ?></p>
-                    </div>
-                    <!-- end project deatils text -->                
+                <div class="col-md-12">
                     <div class="project-detials-date">
                         <p><span>Date : </span><?= Html::encode("{$dateProject}") ?></p>
                         <p><span>Client :  </span>Themforest</p>
                         <p><span>Categories :   </span>portfolio, psd, template, wordpress.</p>
                     </div>
-                    <a href="#!" class="btn-mr waves-effect waves-light">VIEW LIVE</a>
                 </div>
             </div>
         </div>
